9d9a5e0df47d0ae2337ad96fa88aa145fdeabd0c
Mauricio Gnewuch Vieira <mauricio_gnewuch@hotmail.com>
2024-03-21 17:34:43 -0300

    Testando alternativas para o problema do Scipt

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Variáveis Adicionais
-let msg=document.getElementById("msg");
+let msg1=document.getElementById("msg1");
+let msg2=document.getElementById("msg2");
 
 // Variáveis com a história
 let string1 = "Você é um explorador que está em busca do lendário Cálice Sagrado, que todos almejam. Você chega até uma estalgem no meio da estrada e, como já é de noite, resolve dormir por lá.";
 let string2 = "Depois de acertar seu quarto com o dono, resolve ir até a lareira, onde várias pessoas estão cantando, dançando ou apenas apreciando uma boa bebida na mesa, você resolve sentar e apreciar a vista...";
 
 // Código Principal
-Escrever(string1);
-Escrever(string2);
+Escrever(string1,msg1);
+Escrever(string2,msg2);
  
 
 
@@ -30,7 +31,7 @@ Escrever(string2);
 
 // Funções
 // 1. Função de digitação
-function Escrever(string) {
+function Escrever(string,msg) {
   var i = 0; 
     
 function Caracteres() {
